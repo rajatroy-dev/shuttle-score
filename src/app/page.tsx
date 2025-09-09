6763e0c75eea2d575c5f1d9ef6484de97f2c4aa1
Rajat Roy <rajatroy.dev@gmail.com>
2025-09-09 22:51:19 +0530

    WIP Scoring System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,33 @@
 import { useState } from "react";
 
 export default function Home() {
+  const teamNames = {
+    'teamA': 'Team A',
+    'teamB': 'Team B'
+  };
+
   const [score, setScore] = useState({
     teamA: 0,
     teamB: 0
   });
 
-  const [servingSide, setservingSide] = useState<'teamA' | 'teamB'>('teamA');
+  const [servingSide, setservingSide] = useState<ITeam>('teamA');
 
-  const handleScore = () => {
-    setScore({ ...score, [servingSide]: score[servingSide] + 2 });
+  const handleScore = (scoringTeam: ITeam) => {
+    setScore({ ...score, [scoringTeam]: score[scoringTeam] + 2 });
+    setservingSide(scoringTeam);
   }
 
   return (
     <div>
+      <div>Serving Team: {teamNames[servingSide]}</div>
       <div>
         <h3>Team A</h3>
         <p>Player A1</p>
         <p>Player A2</p>
         <p>Score: {score.teamA}</p>
         <div>
-          <button onClick={handleScore}>Score</button>
+          <button onClick={() => handleScore('teamA')}>Score</button>
         </div>
       </div>
       <div>
@@ -31,7 +38,7 @@ export default function Home() {
         <p>Player B2</p>
         <p>Score: {score.teamB}</p>
         <div>
-          <button onClick={handleScore}>Score</button>
+          <button onClick={() => handleScore('teamB')}>Score</button>
         </div>
       </div>
       <div>
@@ -40,3 +47,5 @@ export default function Home() {
     </div>
   );
 }
+
+type ITeam = 'teamA' | 'teamB';
