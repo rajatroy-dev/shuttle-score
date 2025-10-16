d67284c1eb51d263da2d6d9cfc0ab69f1f9bcb1e
Rajat Roy <rajatroy.dev@gmail.com>
2025-10-17 03:18:55 +0530

    WIP Seperate Singles and Doubles

---
+++ src/app/ui/singles-setup.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import { useAppStore } from "@/app/_providers/app-provider";
+import { IAppStore } from "@/app/_stores/app-store";
+import { useRouter } from "next/navigation";
+
+export default function SinglesSetup() {
+  const {
+    teamAPlayerA,
+    setTeamAPlayerA,
+    teamBPlayerA,
+    setTeamBPlayerA,
+  } = useAppStore((state: IAppStore) => state);
+
+  const router = useRouter();
+
+  return (
+    <div className="container mx-auto px-8">
+      <div className="flex flex-row">
+        <div className={`
+          mt-8 mb-4 p-6 
+          basis-full md:basis-1/2
+        `}>
+          <div className="mb-6">
+            <input
+              type="text"
+              id="playerA"
+              value={teamAPlayerA}
+              onChange={(event) => setTeamAPlayerA(event.target.value)}
+              className={`
+                bg-slate-200 dark:bg-slate-800 placeholder:text-slate-500 dark:placeholder:text-slate-400
+                rounded 
+                px-3 py-2 w-full
+                shadow-md
+              `}
+              placeholder="Team A" />
+          </div>
+        </div>
+        <div className={`
+          hidden md:block 
+          basis-1/2  
+        `}></div>
+      </div>
+      <p className="text-center text-xl font-bold my-2">
+        &#11835;
+        VS
+        &#11835;
+      </p>
+      <div className="flex flex-row">
+        <div className={`
+          hidden md:block 
+          basis-1/2  
+        `}></div>
+        <div className={`
+          mt-8 mb-4 p-6 
+          basis-full md:basis-1/2
+        `}>
+          <div className="mb-6">
+            <input
+              type="text"
+              id="playerB"
+              value={teamBPlayerA}
+              onChange={(event) => setTeamBPlayerA(event.target.value)}
+              className={`
+                bg-slate-200 dark:bg-slate-800 placeholder:text-slate-500 dark:placeholder:text-slate-400
+                rounded 
+                px-3 py-2 w-full
+                shadow-md
+              `}
+              placeholder="Team B" />
+          </div>
+        </div>
+      </div>
+
+      <a
+        onClick={(e) => {
+          e.preventDefault();
+          router.push('/match');
+        }}
+        className={`
+          block my-4 w-full py-2 
+          bg-slate-200 dark:bg-slate-800 
+          text-center font-bold
+          shadow-md cursor-pointer
+          rounded
+        `}>
+        START MATCH
+      </a>
+    </div>
+  );
+}
